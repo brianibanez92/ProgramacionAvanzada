152ec6abcf2b129eb9eed27eb05d822b231c3b3d
unknown <alumno@LAB02PC10.labs.extension>
2016-09-06 20:52:31 -0300

    cambios

--- clase_06.09.16/Archivos/main.java
+++ clase_06.09.16/Archivos/main.java
@@ -1,22 +1,42 @@
 package Archivos;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class main {
 
 	public static void main(String[] args) {
 		
+		FileWriter fw; 
+		BufferedWriter bw = null;
+		
 		try 
 		{
-			File f = new File("texto.txt");
-			FileWriter fw = new FileWriter(f);
+			File f = new File("Archivos\\texto.txt");
 			
+			fw = new FileWriter(f);
+			bw = new BufferedWriter(fw);
 			
+			bw.write("Hola pepe		asdasdasd	");
+			
+			//El buffer solo llama a Writer cuando se llena, por eso forzamos la escritura con el flush
+			bw.flush();
 			
 			
 		} catch (Exception e) 
 		{
+		}
+		finally
+		{
+			//Se cierra la escritura del archivo.
+			try {
+				bw.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 
 	}
