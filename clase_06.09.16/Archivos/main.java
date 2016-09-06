671f0f8826f2f975179efd0507e81671453df54a
unknown <alumno@LAB02PC10.labs.extension>
2016-09-06 20:13:19 -0300

    cambios

---
+++ clase_06.09.16/Archivos/main.java
@@ -0,0 +1,24 @@
+package Archivos;
+
+import java.io.File;
+import java.io.FileWriter;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		try 
+		{
+			File f = new File("texto.txt");
+			FileWriter fw = new FileWriter(f);
+			
+			
+			
+			
+		} catch (Exception e) 
+		{
+		}
+
+	}
+
+}
