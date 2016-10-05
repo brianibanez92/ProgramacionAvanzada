817fd8c7a41437f12286a212fb7cd86765b5748a
unknown <alumno@LAB02PC10.labs.extension>
2016-10-04 21:50:59 -0300

    Se termina el ejercicio_18 del apunte

--- Ejercicios_Apuntes/src/cm/utn/ejercicio18/test/MihiloTest.java
+++ Ejercicios_Apuntes/src/cm/utn/ejercicio18/test/MihiloTest.java
@@ -11,10 +11,12 @@ public class MihiloTest {
 		Thread t1=new Thread(new Mihilo(), "Hilo_1");
 		Thread t2=new Thread(new Mihilo(), "Hilo_2");
 		Thread t3=new Thread(new Mihilo(), "Hilo_3");
-		
-		t1.start();
-		t2.start();
-		t3.start();
+		while(t1.isAlive() && t2.isAlive() && t3.isAlive()){
+			t1.start();
+			t2.start();
+			t3.start();
+		}
+		System.out.println("Fin...");
 	}
 
 }
