38458a61399b4b9cc282414a1b80b67d206ca8b8
unknown <alumno@LAB02PC10.labs.extension>
2016-10-18 22:01:38 -0300

    Se sube el servicio de ExecutorService y puedo implementar en android

--- Clase8/src/utn/com/clases/Main.java
+++ Clase8/src/utn/com/clases/Main.java
@@ -1,28 +1,36 @@
 package utn.com.clases;
 
 import java.util.HashSet;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
-
+	/*t1.start();
+	t1.join();
+	t2.start();
+	t2.join();
+	Thread.yield();
+	t2.start();*/
 	@SuppressWarnings("static-access")
 	public static void main(String[] args) throws InterruptedException {
-		ClaseTheard hilo=new ClaseTheard();
+		/*ClaseTheard hilo=new ClaseTheard();
 		Thread t1=new Thread(hilo,"Hilo-1");
-		//Thread t2=new Thread(hilo,"Hilo-2");
-		//Thread t3=new Thread(hilo,"Hilo-3");
-		
-		/*t1.start();
-		t1.join();
-		t2.start();
-		t2.join();
-		Thread.yield();
-		t2.start();*/
 		
 		t1.start();
 		t1.sleep(20);
-		hilo.retomar();
+		hilo.retomar();*/
 		
-		
+		ExecutorService ex=Executors.newFixedThreadPool(5);
+		for(int i=0;i<100;i++){
+			ex.execute(new Runnable() {
+				
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					System.out.println("hola");
+				}
+			});
+		}
 	
 	}
 
