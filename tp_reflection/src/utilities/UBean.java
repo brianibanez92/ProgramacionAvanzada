37c03bb65bcc2123a542624ca185231544aaa48f
unknown <alumno@LAB02PC09.labs.extension>
2016-10-25 21:39:28 -0300

    cambios

---
+++ tp_reflection/src/utilities/UBean.java
@@ -0,0 +1,40 @@
+package utilities;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sun.org.apache.bcel.internal.classfile.Attribute;
+
+public class UBean 
+{
+
+	/*Dentro del paquete “Utilidades” se debe crear una clase llamada UBean, la cual 
+	constara de 3 métodos públicos y estáticos:
+	
+	a.  obtenerAtributos(Object): Devuelve un ArrayList<Field> con todos los atributos 
+	que posee el parámetro Object.
+	b.  ejecutarSet(Object o, String att, Object valor): Se debe ejecutar el método
+	Setter del String dentro del Object.
+	c.  ejecutarGet(Object o, String att): devolverá el valor del atributo pasado por 
+	parámetro, ejecutando el getter dentro del objeto */
+
+	public static List<Field> getAttributes (Object obj)
+	{
+		return new ArrayList<Field>();
+	}
+	
+	public static void executeSetAttribute (Object obj, String attribute, Object value)
+	{
+		
+	}
+	
+	public static Object executeGetAttribute (Object obj, String attribute)
+	{
+		return new Object();
+	}
+	
+	
+	
+	
+}
