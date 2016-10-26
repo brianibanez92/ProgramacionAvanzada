24be7fefdd823a95cf813f09021cb1b40fdc7a63
unknown <alumno@LAB02PC10.labs.extension>
2016-10-25 21:42:24 -0300

    Se sube el esqueleto del package utilidades de trabajo practico

---
+++ Trabajo_Practico/src/utilidades/UBean.java
@@ -0,0 +1,17 @@
+package utilidades;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+public class UBean {
+	//FIXME Hay que implementar todos estos metodos
+	public static ArrayList<Field> obtenerAtributos(Object obj){
+		return null;
+	}
+	public static void ejecutarSet(Object o, String att, Object valor){
+		
+	}
+	public static Object ejecutarGet(Object o, String att){
+		return null;
+	}
+}
