96399287e7ddf6e3d4aa0e0038f3686ccf61a875
Julian.moreno <julian.moreno@globallogic.com>
2016-10-30 23:48:21 -0300

    Se sube lo hecho hasta el momento

--- Trabajo_Practico/src/utilidades/UBean.java
+++ Trabajo_Practico/src/utilidades/UBean.java
@@ -2,14 +2,17 @@ package utilidades;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class UBean {
-	//FIXME Hay que implementar todos estos metodos
+	//Hay que implementar todos estos metodos
 	public static ArrayList<Field> obtenerAtributos(Object obj){
-		return null;
+		Class c1=obj.getClass();
+		ArrayList<Field> ls=(ArrayList<Field>) Arrays.asList(c1.getDeclaredFields());
+		return ls;
 	}
 	public static void ejecutarSet(Object o, String att, Object valor){
-		
+		Class c1=o.getClass();
 	}
 	public static Object ejecutarGet(Object o, String att){
 		return null;
